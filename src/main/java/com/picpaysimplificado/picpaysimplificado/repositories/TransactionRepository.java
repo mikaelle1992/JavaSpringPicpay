d033478dac3b4eeb51a4ff05dcfafde8ee9db4ab
mikaelle1992 <mikaelle.rubiaps02@gmail.com>
2024-12-28 17:29:33 -0300

    feature: criação do TransactionRepository

---
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/repositories/TransactionRepository.java
@@ -0,0 +1,10 @@
+package com.picpaysimplificado.picpaysimplificado.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.picpaysimplificado.picpaysimplificado.domain.transaction.Transaction;
+
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+}
