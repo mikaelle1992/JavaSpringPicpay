fc2cb3e27402fb371e895dbcfd3d5043f17e8f4c
mikaelle1992 <mikaelle.rubiaps02@gmail.com>
2024-12-29 00:10:25 -0300

    fix: delete do findUserByDocument

--- src/main/java/com/picpaysimplificado/picpaysimplificado/repositories/UserRepository.java
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/repositories/UserRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.picpaysimplificado.picpaysimplificado.domain.user.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-	Optional<User>findUserByDocument(String document);
+	// Optional<User>findUserByDocument(String document);
 	Optional<User>findUserById(Long Id);
 }
