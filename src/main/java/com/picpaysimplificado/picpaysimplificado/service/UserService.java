65a700c218a26713f9b4bc713a69a52201efba3f
mikaelle1992 <mikaelle.rubiaps02@gmail.com>
2025-01-02 23:12:30 -0300

    feature: createUser  no service

--- src/main/java/com/picpaysimplificado/picpaysimplificado/service/UserService.java
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/service/UserService.java
@@ -1,10 +1,12 @@
 package com.picpaysimplificado.picpaysimplificado.service;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.picpaysimplificado.picpaysimplificado.DTO.form.UserForm;
 import com.picpaysimplificado.picpaysimplificado.domain.user.User;
 import com.picpaysimplificado.picpaysimplificado.domain.user.Usertype;
 import com.picpaysimplificado.picpaysimplificado.infra.exception.*;
@@ -33,5 +35,19 @@ public class UserService {
    public void saveUser(User user){
         this.repository.save(user);
    }
+
+   public User createUser(UserForm user){
+        // repository.findUserByCpf(user.cpf());
+        if(repository.findUserByCpf(user.cpf()).isPresent()){
+            throw new EntityNotFoundException("Já existe Usuário esse cpf: " + user.cpf());
+        }
+        User userNew =  new User(user);
+        this.saveUser(userNew);
+        return userNew;
+   }
+
+    public List<User> getAllUser() {
+        return this.repository.findAll();
+    }
 
 }
