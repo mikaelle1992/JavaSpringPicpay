a72767e6655e626a8317081f74957974f52e2835
mikaelle1992 <mikaelle.rubiaps02@gmail.com>
2025-01-06 23:26:27 -0300

    feature: arquivos sql

---
+++ src/main/resources/db/data/S01__insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.users
+(balance, cpf, email, first_name, last_name, "password", usertype)
+VALUES(10, '11111111111', 'email-teste@gmail.com', 'Admin', 'admin last_name', '123456', 'MERCHAN');
